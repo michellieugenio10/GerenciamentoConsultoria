4b088ea29a8256ddfbb9c93903df7dcd8f07412f
michelli.eugenio <michelli.eugenio@fiap.com.br>
2020-09-06 19:14:52 -0300

    criação do projeto

---
+++ GerenciamentoConsultoria.DAL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GerenciamentoConsultoria.DAL
+{
+    public class Class1
+    {
+    }
+}
---
+++ GerenciamentoConsultoria/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GerenciamentoConsultoria.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
